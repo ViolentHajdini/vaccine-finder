4021fc03625f600a533f84ec5f8daaba9d6e5928
Darren <darrenzhang200027@gmail.com>
2021-04-25 11:12:28 -0400

    added mongodb connection

---
+++ backend/index.js
@@ -0,0 +1,31 @@
+const express = require("express")
+const app = express()
+const port = process.env.PORT || 5000
+const path = require('path')
+
+// parse application/x-www-form-encoded and application/json
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded())
+app.use(bodyParser.json())
+
+//connect to mongoose
+const mongoose = require("mongoose")
+mongoose.connect(
+    "mongodb+srv://testuser:test123@cluster0.mbd3f.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    { useNewUrlParser: true }
+)
+
+const db = mongoose.connection
+db.on("error", console.error.bind(console, "connection error:"))
+db.once("open", () => {
+    console.log("connected to db")
+})
+
+app.get("/", (req, res) => {
+    console.log(req)
+    res.send("hello")
+})
+
+// app.use('/users', users)
+
+app.listen(port, () => console.log("listening at port", port))
