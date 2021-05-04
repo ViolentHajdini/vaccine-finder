b90c7b843993b7de798f4444821eb54108a2082c
Darren <darrenzhang200027@gmail.com>
2021-05-04 12:43:16 -0400

    added aptment redux

--- frontend/src/redux/appointmentReducer.js
+++ frontend/src/redux/appointmentReducer.js
@@ -11,21 +11,14 @@ const initialState = {
 function vaccineReducer(state = initialState, action) {
   switch (action.type) {
     case ADD_APPOINTMENT:
+        
       let newState = {
         ...state, 
-        vaccine: action.appointment.vaccine,
-        vaccinationSite: action.appointment.name,
-        vaccinationSiteNumber: action.appointment.phoneNumber,
+        ...action.appointment,
         hasAppointmentScheduled: true
       }
+
       return newState
-      // console.log('action appz', action.appointment.vaccine, action.appointment.name, action.appointment.phoneNumber)
-      // return Object.assign({}, state, {
-      //   vaccine: action.appointment.vaccine,
-      //   vaccinationSite: action.appointment.name,
-      //   vaccinationSiteNumber: action.appointment.phoneNumber,
-      //   hasAppointmentScheduled: true
-      // })
     
     // if the action is not recognized, don't do anything
     default:
